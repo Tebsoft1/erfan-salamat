5acc168b6497c789914217d78ffee21168d3c836
Ashkan <tebsoft8@gmail.com>
2025-08-11 15:57:37 +0430

    making some changes
    Co-Authored-By: Ashkan <159788694+ashkanghadimii@users.noreply.github.com>

--- src/pages/Services/components/StatsSection.tsx
+++ src/pages/Services/components/StatsSection.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
+import Container from '@/assets/images/Container.png';
+import Container2 from '@/assets/images/Container2.png';
+import DoctorShape from '@/assets/images/DoctorShape.png';
+
 
 const StatsSection: React.FC = () => {
   return (
-    <div className="w-full max-w-4xl p-4 flex justify-around">
-      <div className="bg-teal-500 p-4 rounded-lg text-center">
-        <span className="text-2xl">3945</span>
-        <p>تعداد بیماران</p>
+    <div className="w-full max-w-4xl flex mt-4">
+      {/* Left Column for DoctorShape Image */}
+      <div className="flex-1 flex flex-col justify-center items-right">
+        <img
+          src={Container2}
+          alt="Container 2"
+          className="w-30 h-33 rounded-4xl p-2 mb-2"
+        />
+
+        <img
+          src={Container}
+          alt="Container"
+          className="w-30 h-33 rounded-4xl p-2"
+        />
       </div>
-      <div className="bg-teal-500 p-4 rounded-lg text-center">
-        <span className="text-2xl">345</span>
-        <p>درمان فعال</p>
-      </div>
+
+      {/* Right Column for Container Images */}
+      <div className="flex-1 flex justify-center items-start">
+        <img
+          src={DoctorShape}
+          alt="Doctor Shape"
+          className="w-80 h-75 rounded-4xl p-2"
+        />
+    </div>
+
     </div>
   );
 };
+
+
 
 export default StatsSection;
