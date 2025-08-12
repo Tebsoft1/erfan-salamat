a2f3dbc320a6b800592e5dd71141a48f542ffede
Ashkan <tebsoft8@gmail.com>
2025-08-12 16:45:28 +0200

    changes
    Co-Authored-By: Ashkan <159788694+Ashkanghadimii@users.noreply.github.com>

--- src/pages/Services/doctor/Doctor.tsx
+++ src/pages/Services/doctor/Doctor.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom' // اگر از React Router استفاده می‌کنید
 import HeaderDoctor from './components/HeaderDoctor'
-import Footer from './components/Footer'
 
 
 const Doctor: React.FC = () => {
@@ -9,7 +8,6 @@ const Doctor: React.FC = () => {
   return (
     <div className=" text-white flex flex-col items-center w-full">
       <HeaderDoctor />
-      <Footer />
       
     </div>
   )
