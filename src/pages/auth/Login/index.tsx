38e4e47f15c01b9fc71cbfe82676f014df5c6fdf
abbasian <abbasian@thn.com>
2025-08-15 10:17:48 +0330

    work on map

--- src/pages/auth/Login/index.tsx
+++ src/pages/auth/Login/index.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import OTPForm from '../components/OTPForm'
 import LoginForm from './components/LoginForm'
+import Map from '@/components/Map'
 
 const Login = () => {
   const [isLoginForm, setIsLoginForm] = useState(true)
   const [mobileNumber, setMobileNumber] = useState<string>('')
 
+  return <Map />
   if (isLoginForm)
     return (
       <LoginForm
