15099b67e16c81d1cbba87732bcade8f06919284
abbasian <abbasian@thn.com>
2025-08-29 07:33:30 +0330

    some changes

--- src/components/GuestRoutes.tsx
+++ src/components/GuestRoutes.tsx
@@ -13,10 +13,7 @@ export default function GuestRoutes() {
   }
 
   return (
-    <div
-      className="flex flex-col items-center flex-1 w-full p-4 mt-10 rounded-tr-4xl rounded-tl-4xl text-primary-900 relative
-     bg-gradient-to-b from-secondary-100 via-secondary-100 to-primary-700 [background:linear-gradient(to_bottom,theme(colors.secondary.100)_0%,theme(colors.secondary.100)_80%,theme(colors.primary.700)_100%)]"
-    >
+    <div className="flex flex-col items-center flex-1 w-full p-4 mt-10 rounded-tr-4xl rounded-tl-4xl text-primary-900 relative bg-secondary-100">
       {isAuthenticated === null ? <BeatLoaderComponent /> : <Outlet />}
     </div>
   )
